49757d1a8aee7a389e91167b319505cfb475f396
Ashutosh <iashutoshmishra8@gmail.com>
2021-07-19 20:15:46 +0530

    Project Finished

---
+++ challenges/02 - JS and CSS Clock/script.js
@@ -0,0 +1,28 @@
+const secondHand = document.querySelector('.second-hand')
+const minuteHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+function setSeconds() {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const secondsDegrees = (6 * seconds) + 90
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+}
+
+function setMinutes() {
+    const now = new Date()
+    const minutes = now.getMinutes()
+    const minutesDegrees = (6 * minutes) + 90
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+}
+
+function setHours() {
+    const now = new Date()
+    const hours = now.getHours()
+    const hoursDegrees = (30 * hours) + 90
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+}
+
+setInterval(setSeconds, 1000)
+setInterval(setMinutes, 1000)
+setInterval(setHours, 1000)
